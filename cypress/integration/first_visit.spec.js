2b9e16b8d766790dd5f9a3adace4f049aa13b393
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-18 19:41:51 -0300

    first_visit test the topbar and the chart

--- cypress/integration/first_visit.spec.js
+++ cypress/integration/first_visit.spec.js
@@ -2,4 +2,23 @@ it('Gus first impressions', () => {
   // Gus wants to test a new webapp to get information about Deezer catalog
   cy.visit('/')
 
+  // He notes the chart list...
+  cy.dataCy('chart')
+    .should('be.visible')
+
+  // and the search field.
+  cy.dataCy('topbar')
+    .should('be.visible')
+
+  // Gus look at the list items and notice...
+  // the music information,
+
+  // (album cover, music title, singer, duration)
+
+  // the full song button,
+
+  // the play/pause button,
+
+  // and the add to favs button.
+
 })
