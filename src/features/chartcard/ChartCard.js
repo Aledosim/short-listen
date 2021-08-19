1379443081cebbc0862cfce90933669040740027
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-19 02:10:33 -0300

    Add ChartCard

---
+++ src/features/chartcard/ChartCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export function songTime(totalTime) {
+  const min = Math.floor(totalTime / 60)
+  const sec = totalTime % 60
+
+  return `${min}:${String(sec).padStart(2, "0")}`
+}
+
+export default function ChartCard(props){
+
+  return(
+    <div>
+      <img alt="Album cover" src={props.cover} />
+      {props.title}
+      {props.singer}
+      {songTime(props.time)}
+    </div>
+  );
+};
+
