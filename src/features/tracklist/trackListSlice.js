40efea819c7c0e2568892908c942916d80a6ae2b
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-20 08:41:53 -0300

    Correct trackListSlice fixture import

--- src/features/tracklist/trackListSlice.js
+++ src/features/tracklist/trackListSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 import { fetchChart } from './trackListAPI';
-import chartFixture from '../../fixtures/chartFixture'
+import chartFixture from './__fixtures__/chartFixture'
 
 const initialState = {
   value: [],
