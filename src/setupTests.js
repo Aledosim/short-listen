769db633d685cf1a3bdbbca166fc8127b1822997
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-20 10:31:33 -0300

    Configure tests to use axios mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+
+import chartFixture from './__fixtures__/chartFixture'
+
+var axiosMock = new MockAdapter(axios);
+const data = chartFixture
+axiosMock.onGet('chart').reply(200, data);
+
+global.axios = axiosMock
