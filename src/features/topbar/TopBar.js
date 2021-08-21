7ab24a7f5b6713f207c5fd1a5aa2894cb8ffa297
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-21 10:49:56 -0300

    Delete old search function of TopBar

--- src/features/topbar/TopBar.js
+++ src/features/topbar/TopBar.js
@@ -1,23 +1,5 @@
 import React, { useContext } from 'react'
 
-export function search(setCurrency) {
-  const searchInner = (event) => {
-    const searchField  = document.getElementById('searchField')
-    const text = searchField.value.toLowerCase()
-
-    if (Object.keys(configs.currencies).includes(text)) {
-      setCurrency(text)
-      searchField.value = ''
-    } else {
-      searchField.value = ''
-      searchField.placeholder = 'Termo inválido'
-    }
-    event.preventDefault()
-  }
-
-  return searchInner
-}
-
 export default function TopBar(){
 
   return(
