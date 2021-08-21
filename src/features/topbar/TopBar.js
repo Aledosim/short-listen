e5937430e0fc7bd36b16f92e2b5d9a44522da7a9
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-21 18:29:19 -0300

    Add style to TopBar from old css

--- src/features/topbar/TopBar.js
+++ src/features/topbar/TopBar.js
@@ -1,28 +1,88 @@
 import React from 'react'
+import styled from 'styled-components'
+import style from 'styled-components'
 
 import { ReactComponent as Logo } from './deezer_logo.svg'
 
+const Container = style.header`
+  display: inline-flex;
+  align-items: center;
+
+  width: 100%;
+  max-height: 50px;
+  padding-top: 0.75rem;
+  padding-bottom: 0.5rem;
+
+`
+
+const StyledLogo = styled(Logo)`
+  margin-left: 2.5rem;
+  max-width: 100px;
+  height: auto;
+`
+
+const Form = style.form`
+  position: relative;
+
+  margin: auto;
+
+  width: 30rem;
+  height: 3rem;
+
+  display: flex;
+  align-items: center;
+`
+
+const Input = style.input.attrs(() => {})`
+  & {
+    width: 100%;
+    height: 100%;
+    padding-right: 4rem;
+    padding-left: 2rem;
+
+    background-color: #F4F1F1;
+    border: none;
+    border-radius: 4px;
+  }
+
+  &::placeholder {
+    color: #000000, 46%;
+  }
+`
+
+const Button = style.button`
+  position: relative;
+  transform: translateX(-100%);
+  right: 9px;
+
+  width: 13px;
+  height: 14px;
+  border: none;
+
+  background-color: #F4F1F1;
+  color: #9B9B9B;
+`
 export default function TopBar(){
 
   return(
-    <header data-cy='topbar'>
-      <Logo />
-      <form
+    <Container data-cy='topbar'>
+      <StyledLogo />
+      <Form
         onSubmit={() => {}}
       >
-      <input
-        type='search'
-        autoComplete='on'
-        autoSave='true'
-        placeholder='Album, artist or title'
-        id='searchField'
-        data-cy='searchField'
-      />
-      <button
-        type='submit'
-        data-cy='searchButton'
-      />
-      </form>
-    </header>
+        <Input
+          type='search'
+          autoComplete='on'
+          autoSave='true'
+          placeholder='Album, artist or title'
+          id='searchField'
+          data-cy='searchField'
+        />
+        <Button
+          type='submit'
+          data-cy='searchButton'
+        />
+      </Form>
+    </Container>
   );
 };
