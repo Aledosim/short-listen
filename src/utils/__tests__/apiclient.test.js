fd4e195e1197f87b6611cf1855b5d2f39db235d4
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-21 10:58:48 -0300

    Fix fetchTracksChart arguments

--- src/utils/__tests__/apiclient.test.js
+++ src/utils/__tests__/apiclient.test.js
@@ -3,6 +3,7 @@ import { fetchTracksChart } from '../apiclient'
 describe('fetchTracksChart tests', () => {
   it('should return a valid value', async () => {
     const actual = await fetchTracksChart()
+
     expect(actual).toBeDefined()
   })
 })
--- src/utils/apiclient.js
+++ src/utils/apiclient.js
@@ -2,7 +2,14 @@ import axios from 'axios'
 
 axios.defaults.baseURL = "https://api.deezer.com/"
 
-export async function fetchTracksChart(index=0, limit=10){
+export async function fetchTracksChart(options){
+  var index
+  var limit
+  if (options) {
+    index = options.index
+    limit = options.limit
+  }
+
   const params = {
     index: index,
     limit: limit
@@ -12,6 +19,7 @@ export async function fetchTracksChart(index=0, limit=10){
     const response = await axios.get('chart', { params: params })
     return response.data.tracks
   } catch (error) {
+    console.log("Something got really bad")
     console.log(error)
   }
 }
