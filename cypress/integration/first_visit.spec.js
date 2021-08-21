ce2659ec9264257078cde59b1eb2d4db3c6a8b64
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-21 17:03:16 -0300

    Modifies first_visit integration test

--- cypress/integration/first_visit.spec.js
+++ cypress/integration/first_visit.spec.js
@@ -3,22 +3,25 @@ it('Gus first impressions', () => {
   cy.visit('/')
 
   // He notes the chart list...
-  cy.dataCy('chart')
+  cy.findByRole('list')
     .should('be.visible')
 
   // and the search field.
-  cy.dataCy('topbar')
+  cy.findAllByRole('listitem')
     .should('be.visible')
+    .within((items) => {
+      // Gus look at the list items and notice...
 
-  // Gus look at the list items and notice...
-  // the music information,
+      // the music information,
+      // (album cover, music title, singer, duration)
 
-  // (album cover, music title, singer, duration)
+      // the full song button,
 
-  // the full song button,
+      // the play/pause button,
 
-  // the play/pause button,
+      // and the add to favs button.
 
-  // and the add to favs button.
+    })
+
 
 })
