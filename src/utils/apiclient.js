5492a1546dc46af4e99f10dba35f3ba8f4c3c55a
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-22 01:30:22 -0300

    Fix fetchTrackChart parameters

--- src/utils/apiclient.js
+++ src/utils/apiclient.js
@@ -2,21 +2,10 @@ import axios from 'axios'
 
 axios.defaults.baseURL = "https://api.deezer.com/"
 
-export async function fetchTracksChart(options){
-  var index
-  var limit
-  if (options) {
-    index = options.index
-    limit = options.limit
-  }
-
-  const params = {
-    index: index,
-    limit: limit
-  }
+export async function fetchTracksChart({index, limit} = {index: undefined, limit: undefined}){
 
   try {
-    const response = await axios.get('chart', { params: params })
+    const response = await axios.get('chart', { params: {index, limit} })
     return response.data.tracks
   } catch (error) {
     console.log("Something got really bad")
