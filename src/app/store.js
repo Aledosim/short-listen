b3c63b6a6035a53f63feac47f8b15935f40dc251
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-21 22:32:26 -0300

    Create searchTermReducer basic structure

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import trackListReducer from '../features/tracklist/trackListSlice'
+import searchTermReducer from '../features/topbar/searchSlice'
 
 export const store = configureStore({
   reducer: {
     trackList: trackListReducer,
+    searchTerm: searchTermReducer,
   },
 });
