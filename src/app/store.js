bd1a18566abaf1e5587d72d3982b786bb1eacf8a
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-22 16:21:05 -0300

    Remove searchTermReducer from store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import trackListReducer from '../features/tracklist/trackListSlice'
-import searchTermReducer from '../features/topbar/searchSlice'
 
 export const store = configureStore({
   reducer: {
     trackList: trackListReducer,
-    searchTerm: searchTermReducer,
   },
 });
