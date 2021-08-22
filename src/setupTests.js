dc5020edf1c872d2754ec4e12683e1c022404570
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-22 01:27:03 -0300

    Configure search API mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,18 +2,20 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
 import '@testing-library/jest-dom/extend-expect';
 import MockAdapter from 'axios-mock-adapter'
 import 'jest-styled-components'
 
 import axios from 'axios'
 import chartFixture from './__fixtures__/chartFixture'
+import searchFixture from './__fixtures__/searchFixture.json'
 
 var axiosMock = new MockAdapter(axios);
-const data = chartFixture
 beforeEach(() => {
   axiosMock.reset()
-  axiosMock.onGet('chart').reply(200, data);
+  axiosMock.onGet('chart').reply(200, chartFixture);
+  axiosMock.onGet('search').reply(200, searchFixture);
 })
 
 global.axios = axiosMock
