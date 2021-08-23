bdd305c92af96168b639336fe68b4ec078b3cf7a
Alexandre do Sim <aledosim@yahoo.com.br>
2021-08-23 01:42:48 -0300

    Add appReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import trackListReducer from '../features/tracklist/trackListSlice'
 import favoritesReducer from '../features/favorites/favoritesSlice'
+import appReducer from './appSlice'
 
 export const store = configureStore({
   reducer: {
     trackList: trackListReducer,
     favorites: favoritesReducer,
+    app: appReducer,
   },
 });
